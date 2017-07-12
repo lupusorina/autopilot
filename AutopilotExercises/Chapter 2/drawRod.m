8520aeabd0325b0090ad950c1936bea1eb9d4d3d
Sorina Lupu <lupusorina@yahoo.com>
2017-07-12 12:24:04 +0200

    exercises chapter 2 of the Book Small Unmanned Aircraft

---
+++ AutopilotExercises/Chapter 2/drawRod.m
@@ -0,0 +1,9 @@
+function handle = drawRod(y, theta, L, gap, height, handle, mode)
+    X = [y, y + L*sin(theta)];
+    Y = [gap + height, gap + height + L*cos(theta)];
+    if isempty(handle)
+        handle = fill(X,Y,'g', 'EraseMode', mode);
+    else
+        set(handle, 'XData',X,'Ydata',Y)
+ 
+    end
