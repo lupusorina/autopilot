0052b98df9df327dc92964a16481db09066c1f10
Sorina Lupu <lupusorina@yahoo.com>
2017-09-01 18:23:53 +0300

    reorganization

--- AutopilotExercises/Chapter 2/drawRod.m
+++
@@ -1,9 +0,0 @@
-function handle = drawRod(y, theta, L, gap, height, handle, mode)
-    X = [y, y + L*sin(theta)];
-    Y = [gap + height, gap + height + L*cos(theta)];
-    if isempty(handle)
-        handle = fill(X,Y,'g', 'EraseMode', mode);
-    else
-        set(handle, 'XData',X,'Ydata',Y)
- 
-    end
--- AutopilotExercises/Chapter 2/translate.m
+++
@@ -1,3 +0,0 @@
-function XYZ = translate(XYZ, pn, pe, pd)
-    XYZ = XYZ + repmat([pn;pe;pd], 1, size(XYZ,2));
-    
